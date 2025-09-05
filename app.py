ee85bd6135f54bb77fb1ed0ed859f5da401f608d
Dinesh V Sundaram <dinesh.v.sundaram@gmail.com>
2025-09-06 02:07:17 +0530

    Updated APP for port 5000

--- app.py
+++ app.py
@@ -621,4 +621,4 @@ def surprise_destination():
         return jsonify({'success': False, 'error': f'Error generating surprise destination: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
